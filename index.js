ffe78c2e62be93c1df5366c242d63a37b2125228
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-01-21 10:01:13 -0800

    Larger default window size

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function onClosed() {
 
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
+		width: 1200,
+		height: 600
 	});
 
 	win.loadURL(`file://${__dirname}/public/html/index.html`);
