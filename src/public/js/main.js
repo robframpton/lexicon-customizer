1c76c5ff921a9e3964f4365e3a3c4c7eff55816d
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-02-05 12:31:55 -0800

    Clean up main.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,54 +1,29 @@
 'use strict';
 
-import _ from 'lodash';
-import fs from 'fs';
-import he from 'he';
-import path from 'path';
-
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Autosuggest from 'react-autosuggest';
 
-import componentScraper from '../../../lib/component-scraper';
-import sass from '../../../lib/sass';
-import themeUtil from '../../../lib/theme';
-import UserConfig from '../../../lib/user_config';
-
-import lexiconCustomizerReducer from '../js/reducers/index'
-
-import LexiconCustomizer from '../js/components/LexiconCustomizer'
-
-// <Extract to different file>
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { connect, Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
-const store = createStore(
-	lexiconCustomizerReducer,
-	applyMiddleware(thunk)
-);
-// </Extract to different file>
+import componentScraper from '../../../lib/component-scraper';
+import LexiconCustomizer from '../js/components/LexiconCustomizer'
+import lexiconCustomizerReducer from '../js/reducers/index'
+import UserConfig from '../../../lib/user_config';
 
 let userConfig = new UserConfig();
 
-let lexiconBaseVariables = componentScraper.mapLexiconVariables();
+let initalState = {
+	components: componentScraper.getLexiconBaseComponents(),
+	variables: componentScraper.mapLexiconVariables()
+};
 
-store.dispatch({
-	type: 'SET_VARIABLES',
-	variables: lexiconBaseVariables
-});
-
-// let customVariables = componentScraper.getVariablesFromFile(path.join(process.cwd(), 'lexicon/_custom_variables.scss'));
-// store.dispatch({
-// 	type: 'SET_VARIABLES',
-// 	variables: customVariables
-// });
-
-let components = componentScraper.getLexiconBaseComponents();
-store.dispatch({
-	type: 'SET_COMPONENTS',
-	components
-});
+const store = createStore(
+	lexiconCustomizerReducer,
+	initalState,
+	applyMiddleware(thunk)
+);
 
 const render = () => {
 	ReactDOM.render(
