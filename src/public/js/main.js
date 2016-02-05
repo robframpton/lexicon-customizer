38c313a4027694d1f8ac29ee5fa76bb579d9e650
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-02-05 12:32:30 -0800

    Sort

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,9 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import componentScraper from '../../../lib/component-scraper';
